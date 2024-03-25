efece247135e71d9c91f5f3a9f9a0f9b020ea215
rohansinha01 <rohansinha01@gmail.com>
2024-03-25 00:20:46 -0400

    os

--- people/settings.py
+++ people/settings.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "localkey2024") #replace the SECRET_KEY VARIABLE with this. 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  'RENDER' not in os.environ  # replace the DEBUG variable with this. 
 
 from pathlib import Path
 import dj_database_url
@@ -23,7 +19,11 @@ import os
 
 env=environ.Env()
 environ.Env.read_env()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY", "localkey2024") #replace the SECRET_KEY VARIABLE with this. 
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG =  'RENDER' not in os.environ  # replace the DEBUG variable with this. 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
